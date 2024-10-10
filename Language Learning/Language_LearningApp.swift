1ff8ef5b02e9693d3b8723252fa2a710c72b6a57
David T <matkatdavid@gmail.com>
2024-10-10 08:59:01 -0600

    Initial Commit

---
+++ Language Learning/Language_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Language_LearningApp.swift
+//  Language Learning
+//
+//  Created by David T on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Language_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
